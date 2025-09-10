8dde904f706420312519e935fecfe99757ca04ad
TheSecondComing123 <jiuprogrammer@gmail.com>
2025-09-09 20:05:50 -0400

    Refactor tank controls and cleanup main loop
    Removed joystick support and unused code from main.py, switching tank controls to keyboard input for both players. Cleaned up formatting and improved consistency in Tank.py, including corner calculations and image loading. Minor code style improvements throughout.

--- Tank.py
+++ Tank.py
@@ -7,6 +7,7 @@ import time
 GREEN = (0, 255, 0)
 PI = 3.141592653
 
+
 class Tank:
     def __init__(self, x, y, deg, img):
         self.x = x
@@ -22,7 +23,6 @@ class Tank:
         self.MAXBULLETS = 3
         self.bullets = []
 
-
         self.isSpinCounterClockwise = False
         self.isSpinClockwise = False
         self.isMoveForward = False
@@ -31,30 +31,30 @@ class Tank:
         self.is_alive = True
 
         #              top-left/bottom-left/bottom-right/top-right
-        self.corners = [[self.x - self.height / 2 +5, self.y - self.width/ 2], \
-            [self.x - self.height / 2 +5, self.y + self.width/ 2], \
-            [self.x + self.height / 2 -5, self.y + self.width/ 2], \
-            [self.x + self.height / 2 -5, self.y - self.width/2]]
-        
+        self.corners = [
+            [self.x - self.height / 2 + 10, self.y - self.width / 2],
+            [self.x - self.height / 2 + 10, self.y + self.width / 2],
+            [self.x + self.height / 2 - 10, self.y + self.width / 2],
+            [self.x + self.height / 2 - 10, self.y - self.width / 2],
+        ]
+
         self.corners_offset = self.corners[::]
 
-        self.tank_img = pygame.image.load(os.path.join('Assets', img))
+        self.tank_img = pygame.image.load(os.path.join("Assets", img))
         self.tank_img_copy = pygame.transform.rotate(self.tank_img, 360 - self.deg)
         self.UpdateImage()
-    
+
     def Draw(self, WIN):
         for i in range(len(self.bullets)):
             self.bullets[i].Draw(WIN)
-            
+
         if not self.is_alive:
             return
 
         WIN.blit(self.tank_img_copy, self.rect)
-        '''for i in range(len(self.corners)):
+        """for i in range(len(self.corners)):
             rect = pygame.Rect(self.corners_offset[i][0], self.corners_offset[i][1], 2, 2)
-            pygame.draw.rect(WIN, (255, 0, 0), rect)'''
-        
-        
+            pygame.draw.rect(WIN, (255, 0, 0), rect)"""
 
     def SpinClockwise(self, maze):
         if not self.is_alive or not self.isSpinClockwise:
@@ -84,12 +84,11 @@ class Tank:
             self.UpdateImage()
             self.RotateCorners()
 
-    
     def MoveForward(self, maze):
         if not self.is_alive or not self.isMoveForward:
             return
-        x_pos = self.move_speed * math.cos(self.deg*PI/180)
-        y_pos = self.move_speed * math.sin(self.deg*PI/180)
+        x_pos = self.move_speed * math.cos(self.deg * PI / 180)
+        y_pos = self.move_speed * math.sin(self.deg * PI / 180)
 
         self.x += x_pos
         self.y += y_pos
@@ -107,8 +106,8 @@ class Tank:
     def MoveBackward(self, maze):
         if not self.is_alive or not self.isMoveBackward:
             return
-        x_pos = self.move_speed * math.cos(self.deg*PI/180)
-        y_pos = self.move_speed * math.sin(self.deg*PI/180)
+        x_pos = self.move_speed * math.cos(self.deg * PI / 180)
+        y_pos = self.move_speed * math.sin(self.deg * PI / 180)
 
         self.x -= x_pos
         self.y -= y_pos
@@ -126,17 +125,17 @@ class Tank:
     def TranslateCorners(self, x, y):
         for i in range(len(self.corners)):
             self.corners[i] = [self.corners[i][0] + x, self.corners[i][1] + y]
-    
+
     def RotateCorners(self):
         for i in range(len(self.corners)):
             ox = self.corners[i][0] - self.x
             oy = self.corners[i][1] - self.y
             nx, ny = RotatePoints(ox, oy, self.deg)
-            #print(ox, oy, nx, ny)
+            # print(ox, oy, nx, ny)
             nx += self.x
             ny += self.y
             self.corners_offset[i] = [nx, ny]
-    
+
     def ShootBullet(self):
         if not self.is_alive:
             return
@@ -149,18 +148,17 @@ class Tank:
             self.bullets[i].Update(maze)
             self.bullets[i].CheckCollisionTank(tanks)
         self.RemoveOldBullets()
-    
+
     def RemoveOldBullets(self):
-        for i in range(len(self.bullets)-1, -1, -1):
+        for i in range(len(self.bullets) - 1, -1, -1):
             if self.bullets[i].IsOld():
                 del self.bullets[i]
-
 
     def UpdateImage(self):
         self.tank_img_copy = pygame.transform.rotate(self.tank_img, 360 - self.deg)
         self.rect = self.tank_img_copy.get_rect()
         self.rect.center = (self.x, self.y)
-    
+
     def UpdateMovement(self, gMaze):
         self.SpinCounterClockwise(gMaze)
         self.SpinClockwise(gMaze)
@@ -179,44 +177,42 @@ class Bullet:
         self.dx = self.move_speed * math.cos(self.deg * PI / 180)
         self.dy = self.move_speed * math.sin(self.deg * PI / 180)
 
-        
         self.radius = 3
         self.MAXLIFETIME = 10
 
         self.time_start = time.time()
-    
+
     def Draw(self, WIN):
         pygame.draw.circle(WIN, (0, 0, 0), (self.x, self.y), self.radius)
 
     def SetCorners(self, x, y):
-        return [ [x - self.radius, y - self.radius], \
-                 [x - self.radius, y + self.radius], \
-                 [x + self.radius, y + self.radius], \
-                 [x + self.radius, y - self.radius] ]
-    
+        return [
+            [x - self.radius, y - self.radius],
+            [x - self.radius, y + self.radius],
+            [x + self.radius, y + self.radius],
+            [x + self.radius, y - self.radius],
+        ]
+
     def Update(self, maze):
 
         x_only = False
         y_only = False
         both = False
 
-
         corners = self.SetCorners(self.x + self.dx, self.y)
-        
 
         if maze.CheckCollision(corners):
             x_only = True
-        
+
         corners = self.SetCorners(self.x, self.y + self.dy)
-        
+
         if maze.CheckCollision(corners):
             y_only = True
-        
+
         corners = self.SetCorners(self.x + self.dx, self.y + self.dy)
 
         if maze.CheckCollision(corners):
             both = True
-        
 
         if x_only and not y_only:
             self.dx *= -1
@@ -230,30 +226,20 @@ class Bullet:
             self.y += self.dy
 
     def CheckCollisionTank(self, tanks):
-        bullet_corners = self.SetCorners(self.x , self.y)
-        for i in range(len(tanks)-1, -1, -1):
+        bullet_corners = self.SetCorners(self.x, self.y)
+        for i in range(len(tanks) - 1, -1, -1):
             if not tanks[i].is_alive:
                 continue
             if do_polygons_intersect(bullet_corners, tanks[i].corners_offset):
-                #print("Hit Tank")
+                # print("Hit Tank")
                 tanks[i].is_alive = False
                 self.time_start -= self.MAXLIFETIME
 
-
-
-
-
     def IsOld(self):
         return time.time() - self.time_start >= self.MAXLIFETIME
-    
 
 
-
-
-
-
-
-#will rotate (x, y) about the origin
+# will rotate (x, y) about the origin
 def RotatePoints(x, y, deg):
     nx = x * math.cos(deg * PI / 180.0) - y * math.sin(deg * PI / 180.0)
     ny = x * math.sin(deg * PI / 180.0) + y * math.cos(deg * PI / 180.0)
--- main.py
+++ main.py
@@ -13,9 +13,6 @@ WHITE = (200, 200, 200)
 LIGHT_BLACK = (0, 0, 0)
 FPS = 60
 
-
-pygame.joystick.init()
-joysticks = [pygame.joystick.Joystick(i) for i in range(pygame.joystick.get_count())]
 DEADZONERANGE = 0.5
 
 MAZE_HEIGHT = 8
@@ -25,13 +22,13 @@ gMaze = Maze(MAZE_HEIGHT, MAZE_WIDTH, HEIGHT, WIDTH)
 gTanks = []
 gPlayers = []
 
+
 def draw_window():
     WIN.fill(WHITE)
     gMaze.Draw(WIN)
     for i in range(len(gTanks)):
         gTanks[i].Draw(WIN)
     pygame.display.flip()
-
 
 
 def initialize_my_stuff():
@@ -44,26 +41,28 @@ def initialize_my_stuff():
     gPlayers.append(player1)
     gPlayers.append(player2)
 
-    tank1 = Tank(200, 200, 0, 'green_tank.png')
-    tank2 = Tank(500, 500, 0, 'red_tank.png')
+    tank1 = Tank(200, 200, 0, "green_tank.png")
+    tank2 = Tank(500, 500, 0, "red_tank.png")
 
     gTanks.append(tank1)
     gTanks.append(tank2)
 
-    
 
 def update_bullets():
     for i in range(len(gTanks)):
         gTanks[i].UpdateBullets(gMaze, gTanks)
 
+
 def InDeadzone(num):
     return abs(num) < DEADZONERANGE
+
 
 def CheckWin():
     for tank in gTanks:
         if not tank.is_alive:
             return True
     return False
+
 
 def ResetPosition():
     global gMaze
@@ -72,27 +71,22 @@ def ResetPosition():
     gMaze.RemoveExtraWalls(10)
     for tank in gTanks:
         tank.is_alive = True
-    
-    tankImages = ['green_tank.png', 'red_tank.png']
 
-    # reset player position
+    tankImages = ["green_tank.png", "red_tank.png"]
+
     for i in range(len(gTanks)):
-        randx = random.randrange(2,MAZE_WIDTH + 1) * 100
-        randy = random.randrange(2,MAZE_HEIGHT + 1) * 100
+        randx = random.randrange(2, MAZE_WIDTH + 1) * 100
+        randy = random.randrange(2, MAZE_HEIGHT + 1) * 100
 
         randdeg = random.randrange(360)
 
         gTanks[i] = Tank(randx, randy, randdeg, tankImages[i])
-        
-
 
 
 def main():
-    global joysticks
     initialize_my_stuff()
 
     clock = pygame.time.Clock()
-    
 
     run = True
     while run:
@@ -102,77 +96,22 @@ def main():
                 run = False
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_m:
-                    gTanks[0].fire_key = True
+                    gTanks[0].ShootBullet()
                 if event.key == pygame.K_LCTRL:
-                    gTanks[1].fire_key = True
-        
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_m:
-                    gTanks[0].fire_key = False
-                if event.key == pygame.K_LCTRL:
-                    gTanks[1].fire_key = False
-            
-            if event.type == pygame.JOYBUTTONDOWN:
-                for player in gPlayers:
-                    if player.controller_id == event.instance_id:
-                        if event.button == 0:
-                            gTanks[player.id].ShootBullet()
-                        
-            if event.type == pygame.JOYBUTTONUP:
-                pass
-            if event.type == pygame.JOYAXISMOTION:
-                for player in gPlayers:
-                    if player.controller_id == event.instance_id:
-                        
-                        if event.axis == 0:
-                            if event.value < 0:
-                                gTanks[player.id].isSpinCounterClockwise = True
-                            else:
-                                gTanks[player.id].isSpinClockwise = True
-                            if InDeadzone(event.value):
-                                gTanks[player.id].isSpinCounterClockwise = False
-                                gTanks[player.id].isSpinClockwise = False
-                        
-                        if event.axis == 5:
-                            gTanks[player.id].isMoveForward = True
-                            if event.value < 0:
-                                gTanks[player.id].isMoveForward = False
-                        if event.axis == 4:
-                            gTanks[player.id].isMoveBackward = True
-                            if event.value < 0:
-                                gTanks[player.id].isMoveBackward = False
+                    gTanks[1].ShootBullet()
 
-            if event.type == pygame.JOYHATMOTION: # the d-pad
-                print(event)
-            if event.type == pygame.JOYDEVICEADDED:
-                joysticks = [pygame.joystick.Joystick(i) for i in range(pygame.joystick.get_count())]
-                print(len(joysticks))
-            if event.type == pygame.JOYDEVICEREMOVED:
-                joysticks = [pygame.joystick.Joystick(i) for i in range(pygame.joystick.get_count())]
-                print(len(joysticks))
-            
-        '''
         keys_pressed = pygame.key.get_pressed()
-        if keys_pressed[pygame.K_LEFT]:
-            gTanks[0].isSpinCounterClockwise = True
-        if keys_pressed[pygame.K_RIGHT]:
-            gTanks[0].isSpinClockwise = True
-        if keys_pressed[pygame.K_UP]:
-            gTanks[0].isMoveForward = True
-        if keys_pressed[pygame.K_DOWN]:
-            gTanks[0].isMoveBackward = True
 
-        if keys_pressed[pygame.K_d]:
-            gTanks[1].SpinCounterClockwise(gMaze)
-        if keys_pressed[pygame.K_g]:
-            gTanks[1].SpinClockwise(gMaze)
-        if keys_pressed[pygame.K_r]:
-            gTanks[1].MoveForward(gMaze)
-        if keys_pressed[pygame.K_f]:
-            gTanks[1].MoveBackward(gMaze)
-        '''
+        gTanks[0].isSpinCounterClockwise = keys_pressed[pygame.K_LEFT]
+        gTanks[0].isSpinClockwise = keys_pressed[pygame.K_RIGHT]
+        gTanks[0].isMoveForward = keys_pressed[pygame.K_UP]
+        gTanks[0].isMoveBackward = keys_pressed[pygame.K_DOWN]
 
-        
+        gTanks[1].isSpinCounterClockwise = keys_pressed[pygame.K_a]
+        gTanks[1].isSpinClockwise = keys_pressed[pygame.K_d]
+        gTanks[1].isMoveForward = keys_pressed[pygame.K_w]
+        gTanks[1].isMoveBackward = keys_pressed[pygame.K_s]
+
         for i in range(len(gTanks)):
             gTanks[i].UpdateMovement(gMaze)
         update_bullets()
@@ -181,8 +120,9 @@ def main():
 
         if game_not_in_play:
             ResetPosition()
-    
+
     pygame.quit()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
